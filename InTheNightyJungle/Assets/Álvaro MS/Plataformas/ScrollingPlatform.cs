c1c9dacd50eddcb022288e900170601cfbeb3104
Álvaro Martínez Soto <halvaren@users.noreply.github.com>
2018-11-06 01:47:58 +0100

    Nueva escena con plataformas con texturas que se mueven en scroll
    Van destinadas al nivel de psicodelia :)

---
+++ InTheNightyJungle/Assets/Álvaro MS/Plataformas/ScrollingPlatform.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingPlatform : MonoBehaviour {
+
+    private Material mat;
+    public float speedMax;
+
+    private float speedX;
+    private float speedY;
+    
+    // Use this for initialization
+	void Start () {
+        mat = GetComponent<SpriteRenderer>().material;
+        speedX = Random.Range(0, speedMax);
+        speedY = Random.Range(0, speedMax);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 currentOffset = mat.GetTextureOffset("_MainTex");
+        mat.SetTextureOffset("_MainTex", new Vector2(currentOffset.x + speedX * Time.deltaTime, currentOffset.y + speedY * Time.deltaTime));
+	}
+}
