4bfacc6d7eb96b46022bfd163fdc6085e66fc396
Halvaren <halvaren98@users.noreply.github.com>
2018-11-08 04:12:02 +0100

    Todas las animaciones del entregable terminadas y adaptadas a cualquier esqueleto

---
+++ InTheNightyJungle-mal/Assets/EntregableAnimaciones/AnimationBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationBehaviour : MonoBehaviour {
+
+    public Animator[] anims;
+    
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+        {
+            PlayAnimation("jump");
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            PlayAnimation("run");
+        }
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            PlayAnimation("special");
+        }
+	}
+
+    private void PlayAnimation(string parameter)
+    {
+        for(int i = 0; i < anims.Length; i++)
+        {
+            anims[i].SetTrigger(parameter);
+        }
+    }
+}
