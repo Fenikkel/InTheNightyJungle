28df5344de5a58611d7062bf8986372282401e8c
Halvaren <halvaren98@users.noreply.github.com>
2018-11-10 16:03:58 +0100

    Metido el señor balbuciente y empezada su IA

---
+++ InTheNightyJungle/Assets/Álvaro MS/IA Enemigos/BalbucienteBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalbucienteBehaviour : PhysicsObject {
+
+    private BoxCollider2D influenceZone;
+    public Vector2 sizeInfluenceZone = new Vector2(1, 1);
+
+	// Use this for initialization
+	void Start () {
+        influenceZone = GetComponent<BoxCollider2D>();
+        influenceZone.size = sizeInfluenceZone;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
