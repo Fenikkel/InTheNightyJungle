6838249c8e545219c786df6df01c10663086bb2c
Fenikkel <paufenollosa@gmail.com>
2018-11-16 23:18:41 +0100

    Quick time events prototype
    Still need animation countdown and more

---
+++ InTheNightyJungle/Assets/Sprint2/KeyShowBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyShowBehaviour : MonoBehaviour {
+
+    //public float interpolationTime;
+
+
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+		
+	}
+    public void ShowIn()
+    {
+        transform.GetComponent<Animator>().Play("KeyFadeIn");
+
+        //FALTA MOVERLO CON CODIGO
+        /*
+        Vector3 initialPosition = transform.GetComponent<Transform>().position;
+        Vector3 finalPosition = transform.GetComponent<Transform>().position + new Vector3(0, 1, 0);
+
+        float elapsedTime = 0.0f;
+        while (elapsedTime < interpolationTime)
+        {
+            elapsedTime += Time.deltaTime;
+            GetComponent<Transform>().position = Vector3.Lerp(initialPosition, finalPosition, elapsedTime / interpolationTime);
+        }
+        */
+        
+    }
+    public void ShowOut()
+    {
+        transform.GetComponent<Animator>().Play("KeyFadeOut");
+    }
+}
