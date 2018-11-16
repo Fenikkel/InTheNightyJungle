f59972df5f7d499e1a0ec85a6a7da924b9c86055
al315233 <al315233@uji.es>
2018-11-17 00:18:48 +0100

    Plataformas móviles
    Plataformas

---
+++ InTheNightyJungle/Assets/Maria/PlatformPlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformPlayerController : MonoBehaviour {
+
+    //Controla movimiento personaje + el resto de las mecánicas
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void StartConversation()
+    {
+
+    }
+}
