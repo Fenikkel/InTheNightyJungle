aa65623f912bd51ce50fcf2b7686728edc6c5244
Halvaren <halvaren98@users.noreply.github.com>
2018-11-17 20:14:21 +0100

    Arreglando el script de movimiento

--- InTheNightyJungle/Assets/Final/Scripts/PhysicsObject.cs
+++ InTheNightyJungle/Assets/Final/Scripts/PhysicsObject.cs
@@ -5,28 +5,28 @@ using UnityEngine;
 public class PhysicsObject : MonoBehaviour {
 
     public float minGroundNormalY = .65f;
-    public float initialGravityModifier = 1f;
     protected float gravityModifier;
+    public float initialGravityModifier = 1f;
 
     protected Vector2 targetVelocity;
     protected bool grounded;
     protected Vector2 groundNormal;
     protected Rigidbody2D rb2d;
     protected Vector2 velocity;
-    public ContactFilter2D contactFilter;
+    protected ContactFilter2D contactFilter;
     protected RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
-    protected List<RaycastHit2D> hitBufferList = new List<RaycastHit2D>(16);
+    protected List<RaycastHit2D> hitBufferList = new List<RaycastHit2D> (16);
 
 
-    protected const float minMoveDistance = 0.001f; //Distancia mínima de movimiento para empezar a comprobar colisiones con el objeto
-    protected const float shellRadius = 0.01f; //El shell radius es una distancia que se añadirá a la distancia para realizar el cast de las colisiones, de manera que así que se evita quedarse enganchado dentro de un collider
+    protected const float minMoveDistance = 0.001f;
+    protected const float shellRadius = 0.05f;
 
     void OnEnable()
     {
-        rb2d = GetComponent<Rigidbody2D>();
+        rb2d = GetComponent<Rigidbody2D> ();
     }
 
-    void Start()
+    void Start () 
     {
         initialization();
     }
@@ -43,77 +43,82 @@ public class PhysicsObject : MonoBehaviour {
         contactFilter.SetLayerMask(Physics2D.GetLayerCollisionMask(gameObject.layer)); //Con esto le estamos diciendo que el contactFilter guarde como máscara de layers aquellas layers contra las que puede colisionar el objeto que estamos tratando, que pueden ser definidas cambiando los settings de las físicas 2D de la escena. Una maravilla, la verdad :)
         contactFilter.useLayerMask = true;
     }
-
-    void Update()
+    
+    void Update () 
     {
         targetVelocity = Vector2.zero;
-        ComputeVelocity();
+        ComputeVelocity (); 
     }
 
     protected virtual void ComputeVelocity()
     {
-
+    
     }
 
     void FixedUpdate()
     {
-        velocity += gravityModifier * Physics2D.gravity * Time.deltaTime; //Calculamos la velocidad del objeto físico en función de cómo se ve afectado por la gravedad desde el último frame
+        velocity += gravityModifier * Physics2D.gravity * Time.deltaTime;
         velocity.x = targetVelocity.x;
 
         grounded = false;
 
-        Vector2 deltaPosition = velocity * Time.deltaTime; //Teniendo en cuenta la velocidad total teórica del objeto, calculamos la distancia teórica a recorrer por el objeto en este frame
+        Vector2 deltaPosition = velocity * Time.deltaTime;
 
-        Vector2 moveAlongGround = new Vector2(groundNormal.y, -groundNormal.x); //Calculamos el movimiento sobre una plataforma teniendo en cuenta la inclinación de la misma, de manera que la dirección del movimiento seguirá esa inclinación
+        Vector2 moveAlongGround = new Vector2 (groundNormal.y, -groundNormal.x);
 
         Vector2 move = moveAlongGround * deltaPosition.x;
 
-        Movement(move, false);
+        Movement (move, false);
 
-        move = Vector2.up * deltaPosition.y; //Nos quedamos con la componente Y de la distancia teórica
+        move = Vector2.up * deltaPosition.y;
 
-        Movement(move, true);
+        Movement (move, true);
     }
 
     void Movement(Vector2 move, bool yMovement)
     {
         float distance = move.magnitude;
 
-        if (distance > minMoveDistance) //Si la distancia a recorrer es mayor que la mínima, empezamos a comprobar cosas
-        {
-            int count = rb2d.Cast(move, contactFilter, hitBuffer, distance + shellRadius); //Cuenta y guarda los colliders (solo los pasan el contactFilter) con los que va a colisionar el objeto en dirección asignada (move), en la distancia dada (distance + shellRadius)
-            hitBufferList.Clear();
-            for (int i = 0; i < count; i++)
-            {
-                hitBufferList.Add(hitBuffer[i]);
+        /* if (distance > minMoveDistance) 
+        {*/
+            int count = rb2d.Cast (move, contactFilter, hitBuffer, distance + shellRadius);
+            hitBufferList.Clear ();
+            for (int i = 0; i < count; i++) {
+                hitBufferList.Add (hitBuffer [i]);
             }
 
-            for (int i = 0; i < hitBufferList.Count; i++)
+            for (int i = 0; i < hitBufferList.Count; i++) 
             {
-                Vector2 currentNormal = hitBufferList[i].normal;
-                if (currentNormal.y > minGroundNormalY) //Esta mierda arregla el que un personaje se quede suspendido en el aire cuando colisiona con una plataforma y mantienes el input activo!!!
+                Vector2 currentNormal = hitBufferList [i].normal;
+                if (currentNormal.y > minGroundNormalY) 
                 {
                     grounded = true;
-                    if (yMovement) //Solo se entra en este condicional cuando se trata del movimiento en Y (caer)
+                    if (yMovement) 
                     {
-                        groundNormal = currentNormal; //Guardamos la normal de la plataforma sobre la que está colocado el personaje
+                        groundNormal = currentNormal;
                         currentNormal.x = 0;
                     }
                 }
 
-                float projection = Vector2.Dot(velocity, currentNormal); //Se resta velocidad menos la normal de la collider actual
-                if (projection < 0)
+                float projection = Vector2.Dot (velocity, currentNormal);
+                if(projection == 0 && currentNormal.y < minGroundNormalY) 
                 {
-                    velocity = velocity - projection * currentNormal; //Se contrarresta la velocidad si la velocidad supera a la normal
+                    projection = -1;
+                }
+                if (projection < 0) 
+                {
+                    velocity = velocity - projection * currentNormal;
                 }
 
-                float modifiedDistance = hitBufferList[i].distance - shellRadius;
+                float modifiedDistance = hitBufferList [i].distance - shellRadius;
                 distance = modifiedDistance < distance ? modifiedDistance : distance;
+                print(distance);
             }
 
 
-        }
+        //}
 
-        rb2d.position = rb2d.position + move.normalized * distance; //Cambiamos la posición del RigidBody añadiendo el movimiento teórico calculado previamente
+        rb2d.position = rb2d.position + move.normalized * distance;
     }
+
 }
