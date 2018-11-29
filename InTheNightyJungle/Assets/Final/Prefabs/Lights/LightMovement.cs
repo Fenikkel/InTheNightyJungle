a8a8de85c78f77258714b8857329ccec6ac24261
Halvaren <halvaren98@users.noreply.github.com>
2018-11-29 02:30:52 +0100

    Puestas luces en un nivel y reordenamiento de cosas en el proyecto

---
+++ InTheNightyJungle/Assets/Final/Prefabs/Lights/LightMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightMovement : MonoBehaviour {
+
+	public float angleMin;
+	public float angleMax;
+	public float speed;
+
+	private float currentAngle;
+	private bool direction;
+
+	private Transform tf;
+
+	// Use this for initialization
+	void Start () {
+		tf = GetComponent<Transform>();
+		currentAngle = tf.eulerAngles.x;
+		direction = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(direction)
+		{
+			currentAngle += speed * Time.deltaTime;
+			if(currentAngle >= angleMax)
+			{
+				currentAngle = angleMax;
+				direction = false;
+			}
+		}
+		else
+		{
+			currentAngle -= speed * Time.deltaTime;
+			if(currentAngle <= angleMin)
+			{
+				currentAngle = angleMin;
+				direction = true;
+			}
+		}
+		tf.eulerAngles = new Vector3(currentAngle, 90, 90);
+	}
+}
