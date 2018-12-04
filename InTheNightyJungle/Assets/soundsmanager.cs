6a20775b536e0eabe46e958088d6136846691541
Fran <al361643@uji.es>
2018-12-04 23:21:48 +0100

    sonidos

---
+++ InTheNightyJungle/Assets/soundsmanager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundsmanager : MonoBehaviour {
+
+    public static AudioClip alientoSound, coinSound, dañoSound, esquivarSound, seleccionarSound;
+    static AudioSource audioSrc;
+	// Use this for initialization
+	void Start () {
+        alientoSound = Resources.Load<AudioClip>("Aliento");
+
+        audioSrc = GetComponent<AudioSource>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public static void PlaySound(string clip)
+    {
+        switch (clip){
+        case "Aliento":
+                audioSrc.PlayOneShot(alientoSound);
+                break;
+        }
+    }
+}
