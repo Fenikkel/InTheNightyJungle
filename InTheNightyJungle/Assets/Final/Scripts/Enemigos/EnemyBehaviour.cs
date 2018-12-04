bbac9ffdcb84a74dc2975888eb1cce21cda4924f
Fran <al361643@uji.es>
2018-12-04 20:44:01 +0100

    algo

--- InTheNightyJungle/Assets/Final/Scripts/Enemigos/EnemyBehaviour.cs
+++ InTheNightyJungle/Assets/Final/Scripts/Enemigos/EnemyBehaviour.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class EnemyBehaviour : MonoBehaviour
 
 {
+    public Animator anim;
     public bool death = false;
     public float damage; //Valor entre 0 y 100, que se considerará un porcentaje
+
+    public void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     public float GetDamage()
     {
@@ -17,9 +23,20 @@ public class EnemyBehaviour : MonoBehaviour
     {
 
     }
+
+    public void OnCollisionEnter2D(Collision2D col)
+    {
+       if (col.gameObject.tag.Equals("fire"))
+        {
+            Destroy(col.gameObject);
+            Destroy(gameObject);
+        } 
+    }
     public void Death()
     {
         death = true;
-
+        anim.SetBool("idle", true);
+        gameObject.SetActive(false);
     }
+
 }
--- InTheNightyJungle/Assets/Final/Scripts/ParticleCollisionScript.cs
+++ InTheNightyJungle/Assets/Final/Scripts/ParticleCollisionScript.cs
@@ -6,11 +6,11 @@ public class ParticleCollisionScript : MonoBehaviour {
 
     public ParticleSystem fireSparks; //lo pongo asi por si reutilizamos el script
 
-    private void OnParticleCollision(GameObject other)
+    public void OnParticleCollision(GameObject other)
     {
         //no hace falta comprobar el tag porque ya esta configurado en el particle system para que solo colisione con los layers que le he dicho
         other.gameObject.SetActive(false);
         //activar animacion muerte y desactivar su box colider o lo que sea
-        other.GetComponent<EnemyBehaviour>().Death();
+        //other.GetComponent<EnemyBehaviour>().OnCollisionEnter2D(col);
     }
 }
