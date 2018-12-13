23b0a75fb083e765dce11981934ff3630053931b
Álvaro Martínez Soto <halvaren@users.noreply.github.com>
2018-12-13 23:58:59 +0100

    Mejora en iluminación de los escenarios y mejoras en los menu de inicio y pausa

---
+++ InTheNightyJungle/Assets/Final/Scripts/ParticleWithTrailMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleWithTrailMovement : MonoBehaviour {
+
+	public Vector2 velocity;
+	public float topLimit;
+	public float bottomLimit;
+	public float leftLimit;
+	public float rightLimit;
+
+	// Use this for initialization
+	void Start () {
+		
+		StartCoroutine(Movement());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private IEnumerator Movement()
+	{
+		Vector2 finalPosition = new Vector2(Random.Range(leftLimit, rightLimit), Random.Range(bottomLimit, topLimit));
+		float smoothTime = (finalPosition.magnitude - transform.position.magnitude) / velocity.magnitude;
+
+		while(!transform.position.Equals(finalPosition))
+		{
+			transform.position = Vector2.SmoothDamp(transform.position, finalPosition, ref velocity, smoothTime);
+			yield return null;
+		}
+	}
+}
---
+++ InTheNightyJungle/Assets/tempScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tempScript : MonoBehaviour {
+
+	public Transform[] positions;
+
+	private int currentPosition;
+
+	// Use this for initialization
+	void Start () {
+		currentPosition = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.A))
+		{
+			transform.position = positions[currentPosition].position;
+			currentPosition++;
+			if(currentPosition == positions.Length) currentPosition = 0;
+		}
+	}
+}
