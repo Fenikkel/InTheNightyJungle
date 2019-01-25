553dcbadd27af8aa4a13e65d34fe349af50e812a
marklg5 <43933460+marklg5@users.noreply.github.com>
2019-01-25 18:34:17 +0100

    Tecla UP y DOWN para las puertas

--- InTheNightyJungle/Assets/Final/Scripts/DoorBehaviour.cs
+++ InTheNightyJungle/Assets/Final/Scripts/DoorBehaviour.cs
@@ -8,6 +8,11 @@ public class DoorBehaviour : MonoBehaviour {
     public Transform cameraPosition;
     public ChamberManager chamber;
 
+    bool shownUPkey;
+    bool shownDOWNkey;
+    public float distanceOverHead;
+    public SpriteRenderer key;
+
     public DoorBehaviour nextDoor;
     public int doorType; //0 hacia dentro, 1 derecha, 2 hacia fuera, 3 izquierda
 
@@ -22,12 +27,14 @@ public class DoorBehaviour : MonoBehaviour {
     // Use this for initialization
 	void Start ()
     {
-        GM = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
-        chamber = GetComponentInParent<ChamberManager>();
+        //GM = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+        //chamber = GetComponentInParent<ChamberManager>();
 
         justOne = false;
         enabledDoor = doorType != 4;
-	}
+        StartCoroutine(DisappearDOWNKey(0.5f));
+        StartCoroutine(DisappearUPKey(0.5f));
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -46,6 +53,15 @@ public class DoorBehaviour : MonoBehaviour {
         {
             if(doorType%2 == 1) { 
                 doorSound.Play();
+            }
+
+            if (doorType == 0)
+            {
+                StartCoroutine(AppearUPKey(0.5f));
+            }
+            else if (doorType == 2)
+            {
+                StartCoroutine(AppearDOWNKey(0.5f));
             }
         }
     }
@@ -72,6 +88,107 @@ public class DoorBehaviour : MonoBehaviour {
                 GM.NextLevel();
             }
         }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (doorType == 0 && shownUPkey)
+        {
+            StartCoroutine(DisappearUPKey(0.5f));
+        }
+        else if(doorType==2 && shownDOWNkey)
+        {
+            StartCoroutine(DisappearDOWNKey(0.5f));
+        }
+        
+    }
+
+        private IEnumerator AppearUPKey(float time)
+    {
+        shownUPkey = true;
+
+        float elapsedTime = 0.0f;
+        Vector2 initialPosition = key.GetComponent<Transform>().position;
+        Vector2 finalPosition = new Vector2(initialPosition.x, initialPosition.y + distanceOverHead);
+
+        Color initialColor = key.color;
+        Color finalColor = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+
+        while (elapsedTime < time)
+        {
+            elapsedTime += Time.deltaTime;
+            key.GetComponent<Transform>().position = Vector2.Lerp(initialPosition, finalPosition, elapsedTime / time);
+            key.color = Color.Lerp(initialColor, finalColor, elapsedTime / time);
+            yield return null;
+        }
+        key.GetComponent<Transform>().position = finalPosition;
+        key.color = finalColor;
+    }
+
+    private IEnumerator DisappearUPKey(float time)
+    {
+        shownUPkey = false;
+
+        float elapsedTime = 0.0f;
+        Vector2 initialPosition = key.GetComponent<Transform>().position;
+        Vector2 finalPosition = new Vector2(initialPosition.x, initialPosition.y - distanceOverHead);
+
+        Color initialColor = key.color;
+        Color finalColor = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+
+        while (elapsedTime < time)
+        {
+            elapsedTime += Time.deltaTime;
+            key.GetComponent<Transform>().position = Vector2.Lerp(initialPosition, finalPosition, elapsedTime / time);
+            key.color = Color.Lerp(initialColor, finalColor, elapsedTime / time);
+            yield return null;
+        }
+        key.GetComponent<Transform>().position = finalPosition;
+        key.color = finalColor;
+    }
+
+    private IEnumerator AppearDOWNKey(float time)
+    {
+        shownDOWNkey = true;
+
+        float elapsedTime = 0.0f;
+        Vector2 initialPosition = key.GetComponent<Transform>().position;
+        Vector2 finalPosition = new Vector2(initialPosition.x, initialPosition.y + distanceOverHead);
+
+        Color initialColor = key.color;
+        Color finalColor = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+
+        while (elapsedTime < time)
+        {
+            elapsedTime += Time.deltaTime;
+            key.GetComponent<Transform>().position = Vector2.Lerp(initialPosition, finalPosition, elapsedTime / time);
+            key.color = Color.Lerp(initialColor, finalColor, elapsedTime / time);
+            yield return null;
+        }
+        key.GetComponent<Transform>().position = finalPosition;
+        key.color = finalColor;
+    }
+
+    private IEnumerator DisappearDOWNKey(float time)
+    {
+        shownDOWNkey = false;
+
+        float elapsedTime = 0.0f;
+        Vector2 initialPosition = key.GetComponent<Transform>().position;
+        Vector2 finalPosition = new Vector2(initialPosition.x, initialPosition.y - distanceOverHead);
+
+        Color initialColor = key.color;
+        Color finalColor = new Color(1.0f, 1.0f, 1.0f, 0.0f);
+
+        while (elapsedTime < time)
+        {
+            elapsedTime += Time.deltaTime;
+            key.GetComponent<Transform>().position = Vector2.Lerp(initialPosition, finalPosition, elapsedTime / time);
+            key.color = Color.Lerp(initialColor, finalColor, elapsedTime / time);
+            yield return null;
+        }
+        key.GetComponent<Transform>().position = finalPosition;
+        key.color = finalColor;
     }
 
     public Vector3 GetPosition()
